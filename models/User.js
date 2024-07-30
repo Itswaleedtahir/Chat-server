0d859b3d4e3e8e5e106561924edf3c84f45f1f0b
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2024-07-30 15:05:07 +0500

    chat server

---
+++ models/User.js
@@ -0,0 +1,67 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var user = new Schema({
+    "firstname": {
+        type: String,
+        required: false
+    },
+    "lastname": {
+        type: String,
+        required: false
+    },
+    "email": {
+        type: String,
+        required: true
+    },
+    "image":{
+        type:String,
+    },
+    "password": {
+        type: String,
+        required: true
+    },
+
+    "profilepic": {
+        type: String
+    },
+    "otp":{
+        type:String
+    },
+    "isVerified":{
+        type:Boolean,
+        default:false
+    },
+    "CampsitesJoined": [{
+        type: Schema.Types.ObjectId,
+        ref: 'Campsite'
+      }],
+    "resetToken":{
+        type:String
+    },
+    "subscriptionStatus":{
+        type:String,
+        default:'No subscription'
+    },
+    "isPaid":{
+        type:String,
+        default:false
+    },
+    "subscriptionId":{
+        type:String,
+        default:false
+    },
+    "planId":{
+        type:String
+    },
+    "favourites": [{
+        type: Schema.Types.ObjectId,
+        ref: 'Campsite'
+    }],
+    "wishlist": [{
+        type: Schema.Types.ObjectId,
+        ref: 'Campsite'
+    }]
+});
+
+module.exports = mongoose.model('User', user);
